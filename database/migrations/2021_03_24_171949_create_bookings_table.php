aaec7bf1338241b7addf8d419e453afe60bc7720
Fer Catalano <fer@wcanvas.com>
2021-03-24 18:39:27 -0300

    Wip - booking
    Signed-off-by: Fer Catalano <fer@wcanvas.com>

---
+++ database/migrations/2021_03_24_171949_create_bookings_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBookingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('supplier');
+            $table->string('day');
+            $table->string('pallets');
+            $table->string('time');
+            $table->string('start');
+            $table->string('end');
+            $table->string('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bookings');
+    }
+}
